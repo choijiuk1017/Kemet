4fb44034d67d130a4406621a56dd7c80d06c8294
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2023-05-23 00:35:26 +0900

    플레이어 이동 추가

---
+++ Assets/Scripts/SethMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SethMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+
+    private Rigidbody2D rigid;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(moveInput * moveSpeed, rigid.velocity.y);
+
+        if(moveInput != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+
+
+}
