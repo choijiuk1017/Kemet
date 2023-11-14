bbb586508d15e889657549fe3aab00f826dd93e4
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2023-11-14 23:18:50 +0900

    몬스터 AI 개선 중

--- Assets/Scripts/Monster/RangeMonster.cs
+++ Assets/Scripts/Monster/RangeMonster.cs
@@ -31,74 +31,74 @@ public class RangeMonster : Monster
     void Start()
     {
         state = State.patrol;
-        //StartCoroutine(Thinking());
     }
 
     void Update()
     {
         distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
 
-        if (state == State.patrol)
+        if (distanceToPlayer < 5f)
         {
-            Patrol();
+            Chase();
         }
-
     }
 
     void Patrol()
     {
-        if(state == State.patrol)
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetected.position, Vector2.down, distance);
+
+        anim.SetBool("isWalking", true);
+
+        Debug.DrawRay(groundDetected.position, Vector2.down * 10f, Color.green);
+
+        if (!groundInfo.collider)
         {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-
-            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetected.position, Vector2.down, distance);
-
-            anim.SetBool("isWalking", true);
-
-            Debug.DrawRay(groundDetected.position, Vector2.down * 10f, Color.green);
-
-            if (!groundInfo.collider)
+            if (MonsterDirRight)
             {
-                if (MonsterDirRight)
-                {
-                    transform.eulerAngles = new Vector3(0, -180, 0);
-                    MonsterDirRight = false;
-                }
-                else
-                {
-                    transform.eulerAngles = new Vector3(0, 0, 0);
-                    MonsterDirRight = true;
-                }
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                MonsterDirRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                MonsterDirRight = true;
             }
         }
     }
 
     void Chase()
     {
-        if(state == State.chase)
+        if (distanceToPlayer < 5f)
         {
-            if (distanceToPlayer < 5f)
-            {
-                Vector2 direction = (player.transform.position - transform.position).normalized;
+            Vector2 direction = (player.transform.position - transform.position).normalized;
 
-                transform.Translate(direction * moveSpeed * Time.deltaTime);
-            }
-            else if(distanceToPlayer >= 5f)
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+            if(direction.x < 0f)
             {
-                state = State.patrol;
+                MonsterDirRight = false;
+                MonsterFlip();
             }
+            else if(direction.x > 0f)
+            {
+                MonsterDirRight = true;
+                MonsterFlip();
+            }
+        
+        }
+        else if (distanceToPlayer >= 5f)
+        {
+            state = State.patrol;
         }
     }
 
     void Attack()
     {
-        if(state == State.attack)
+        if (distanceToPlayer < 2f)
         {
-
-            if (distanceToPlayer < 2f)
-            {
-                anim.SetTrigger("isAttack");  
-            }
+            anim.SetTrigger("isAttack");
         }
     }
 
