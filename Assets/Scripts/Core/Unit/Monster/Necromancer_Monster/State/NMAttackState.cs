66d4d3e5740fe0ce86ce7dbe65233f6e5fbe0b3b
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2024-12-14 22:41:39 +0900

    Updating Necromancer Monster's FSM

--- Assets/Scripts/Core/Unit/Monster/Necromancer_Monster/State/NMAttackState.cs
+++ Assets/Scripts/Core/Unit/Monster/Necromancer_Monster/State/NMAttackState.cs
@@ -11,25 +11,99 @@ namespace Core.Unit.Monster.State.NecromancerMonster
 {
     public class NMAttackState : State<NecromancerMonsterAI>
     {
+        private float attackCooldown = 2f;
+        private float timeSinceLastAttack = 0f;
+        private bool isAttacking = false;
+
         public override void Enter(NecromancerMonsterAI entity)
         {
+            Vector2 direction = (entity.necromancerMonster.targetObject.transform.position - entity.transform.position).normalized;
 
+            if(direction.x > 0  && entity.transform.localScale.x < 0)
+            {
+                entity.transform.localScale = new Vector3(Mathf.Abs(entity.transform.localScale.x), entity.transform.localScale.y, entity.transform.localScale.z);
+            }
+            else if(direction.x < 0 && entity.transform.localScale.x > 0)
+            {
+                entity.transform.localScale = new Vector3(-Mathf.Abs(entity.transform.localScale.x), entity.transform.localScale.y, entity.transform.localScale.z);
+            }
+            
+            timeSinceLastAttack = 0f;
+            isAttacking = false;
         }
 
         public override void Execute(NecromancerMonsterAI entity)
         {
+            if(entity.necromancerMonster.isGroggy)
+            {
+                entity.ChangeState(NMMonsterStateType.Groggy);
+                return;
+            }
+
+            if(!entity.necromancerMonster.isAlive)
+            {
+                entity.ChangeState(NMMonsterStateType.Dead);
+                return;
+            }
+
+            float playerDistance = Vector2.Distance(entity.necromancerMonster.targetObject.transform.position, entity.transform.position);
+
+            if(isAttacking)
+            {
+                return;
+            }
 
 
+            timeSinceLastAttack += Time.deltaTime;
+
+            if(playerDistance <= 15f && timeSinceLastAttack >= attackCooldown)
+            {
+                entity.anim.SetTrigger("Attack");
+                timeSinceLastAttack = 0f;
+
+                //랜덤으로 상태 변환
+                int randomValue = UnityEngine.Random.Range(0, 100);
+
+                if (randomValue < 20) // 20% 확률로 Heal 상태로 전환
+                {
+                    entity.ChangeState(NMMonsterStateType.Heal);
+                }
+                else if (randomValue < 50) // 30% 확률로 Teleport 상태로 전환
+                {
+                    entity.ChangeState(NMMonsterStateType.Teleport);
+                }
+                else // 나머지 50% 확률로 Attack 상태로 전환
+                {
+                    entity.ChangeState(NMMonsterStateType.Attack);
+                }
+            }
+            else if(playerDistance > 15f)
+            {
+                entity.ChangeState(NMMonsterStateType.Idle);
+            }
 
         }
 
         public override void Exit(NecromancerMonsterAI entity)
         {
+            isAttacking = false;
         }
 
         public override void OnTransition(NecromancerMonsterAI entity)
         {
 
+        }
+
+        private void Attack()
+        {
+            isAttacking = true;
+            Debug.Log("공격 시작");
+        }
+
+        public void EndAttack()
+        {
+            isAttacking = false;
+            Debug.Log("공격 끝");
         }
     }
 
--- Assets/Scripts/Core/Unit/Monster/Necromancer_Monster/State/NMTeleportState.cs
+++ Assets/Scripts/Core/Unit/Monster/Necromancer_Monster/State/NMTeleportState.cs
@@ -12,18 +12,62 @@ namespace Core.Unit.Monster.State.NecromancerMonster
     {
         public override void Enter(NecromancerMonsterAI entity)
         {
+            Vector2 direction = (entity.necromancerMonster.targetObject.transform.position - entity.transform.position).normalized;
 
+            if (direction.x > 0 && entity.transform.localScale.x < 0)
+            {
+                entity.transform.localScale = new Vector3(Mathf.Abs(entity.transform.localScale.x), entity.transform.localScale.y, entity.transform.localScale.z);
+            }
+            else if (direction.x < 0 && entity.transform.localScale.x > 0)
+            {
+                entity.transform.localScale = new Vector3(-Mathf.Abs(entity.transform.localScale.x), entity.transform.localScale.y, entity.transform.localScale.z);
+            }
+
+            entity.anim.SetTrigger("Teleport");
         }
 
         public override void Execute(NecromancerMonsterAI entity)
         {
+            if (entity.necromancerMonster.isGroggy)
+            {
+                entity.ChangeState(NMMonsterStateType.Groggy);
+                return;
+            }
 
+            if (!entity.necromancerMonster.isAlive)
+            {
+                entity.ChangeState(NMMonsterStateType.Dead);
+                return;
+            }
+
+            float playerDistance = Vector2.Distance(entity.necromancerMonster.targetObject.transform.position, entity.transform.position);
+
+            if (playerDistance <= 15f)
+            {
+                //랜덤으로 상태 변환
+                int randomValue = UnityEngine.Random.Range(0, 100);
+
+                if (randomValue < 20) // 20% 확률로 Heal 상태로 전환
+                {
+                    entity.ChangeState(NMMonsterStateType.Heal);
+                }
+                else if (randomValue < 50) // 30% 확률로 Teleport 상태로 전환
+                {
+                    entity.ChangeState(NMMonsterStateType.Teleport);
+                }
+                else // 나머지 50% 확률로 Attack 상태로 전환
+                {
+                    entity.ChangeState(NMMonsterStateType.Attack);
+                }
+            }
 
 
         }
 
         public override void Exit(NecromancerMonsterAI entity)
         {
+
+            entity.anim.SetTrigger("TeleportEnd");
         }
 
         public override void OnTransition(NecromancerMonsterAI entity)
